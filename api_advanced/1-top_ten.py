47d7cc93d4c1b89ad0dd368c6e1b1dc37b47cee4
Umukundwa004 <v.umukundwa@alustudent.com>
2025-02-21 16:39:00 +0000

    function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,16 @@
+mport requests
+
+
+def top_ten(subreddit):
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Chrome/1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    # print(response)
+    print(response.text[114839:])
+    if response.status_code != 200:
+        print(None)
+    else:
+        posts = response.json().get('data').get('children')
+        for post in posts:
+            print(post['data']['title'])
