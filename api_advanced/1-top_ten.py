530e4077d8e84b811042e02c028378bd83352bcc
Umukundwa004 <v.umukundwa@alustudent.com>
2025-02-21 20:56:45 +0000

    function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,35 +1,16 @@
 #!/usr/bin/python3
+""" 1-top_ten.py """
 import requests
 
-def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
-    
-    Args:
-        subreddit (str): The name of the subreddit to query.
-    
-    Returns:
-        None
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'Mozilla/5.0'}  # Set a custom User-Agent to avoid 429 errors
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            posts = data['data']['children']
-            for post in posts:
-                print(post['data']['title'])
-        else:
-            print(None)
-    except requests.RequestException:
-        print(None)
 
-# Example usage:
-if __name__ == '__main__':
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
+def top_ten(subreddit):
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print(None)
+        return
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
